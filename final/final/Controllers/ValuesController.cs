582ed911ed7eed86dd7dbe3842f5f9a7f1eceb06
Leonardo Noya garcia <a2019115506@estudiantes.upsa.edu.bo>
2020-12-12 11:38:22 -0400

    terminado

---
+++ final/final/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace final.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Operaciones : ControllerBase
+    {
+        [HttpGet]
+
+        public string op(int numero)
+        {
+            if (numero == 0)
+                return "Realizado por Leonardo";
+            else if (numero < 0)
+                return "ERROR";
+            else
+                return "https://image.freepik.com/vector-gratis/numeros-cartel-imagen_1639-6453.jpg";
+
+        }
+    }
+}
